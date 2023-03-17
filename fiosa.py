76b7c97b214b4d329abadb1ab0819970eadc34e8
DTD-123 <danielhammer92@outlook.com>
2023-03-17 22:28:33 +0400

    UI changes + memory changes

--- fiosa.py
+++ fiosa.py
@@ -1,13 +1,12 @@
 import openai
 import tkinter as tk
+from tkinter import ttk
 import json
 import re
 import threading
 import queue
 import subprocess
-import shutil
-
-
+from datetime import date
 
 longterm_memories_file = open("LongTermMemories.txt", 'r')
 longterm_memories = longterm_memories_file.read()
@@ -53,7 +52,7 @@ data = json.load(f)
 
 commandPattern = r"\$\((.*?)\)"
 
-prompt_to_inject = data['prompt_to_inject'] + longterm_memories # The AI's memories
+prompt_to_inject = data['prompt_to_inject'] + longterm_memories + "\nAdditional information: the current date is " + str(date.today()) # The AI's memories + the date.
 conversation_history = ""
 prompt_queue = queue.Queue()
 command_queue = queue.Queue()
@@ -74,13 +73,20 @@ class ChatWindow:
         self.chat_log = tk.Text(master)
         self.chat_log.pack(fill=tk.BOTH, expand=1)
 
-        self.message_entry = tk.Entry(master)
+        style = ttk.Style()
+        style.configure("Custom.TEntry", fieldbackground='#252525', background='#252525', insertcolor='white', font=("Roboto", 12), padding=5)
+        self.message_entry = ttk.Entry(master, style="Custom.TEntry")
         self.message_entry.pack(side=tk.LEFT, padx=5, pady=5, fill=tk.X, expand=1)
-
-        self.send_button = tk.Button(master, text="Send", command=self.send_message)
+        self.message_entry.configure(width=30)
+        style = ttk.Style()
+        style.configure("Custom.TButton", foreground="#ffffff", background="#339af0", font=("Roboto", 12), padding=5, width=8)
+        self.send_button = ttk.Button(master, text="Send", command=self.send_message, style="Custom.TButton")
         self.send_button.pack(side=tk.RIGHT, padx=5, pady=5)
+  
 
-        self.chat_log.insert(tk.END, "Fiosa: Welcome to l.AInux. Hello, I'm Fiosa, your friendly neighborhood digital assistant here to help with any computer needs. So, tell me, how can I help you?")
+        welcome = run_prompt(prompt_to_inject, "System: Write a couple of sentences introducing yourself to the user. Make sure to ask for their name so you know who they are.", "gpt-3.5-turbo")
+
+        self.chat_log.insert(tk.END, welcome.choices[0].message.content)
         self.message_entry.delete(0, tk.END)
 
     def send_message(self):
@@ -95,7 +101,7 @@ class ChatWindow:
 
         self.chat_log.insert(tk.END, completion.choices[0].message.content)
         self.message_entry.delete(0, tk.END)
-        conversation_history = conversation_history + "\n" + "ChatGPT: " + completion.choices[0].message.content
+        conversation_history = conversation_history + "\n" + "Fiosa: " + completion.choices[0].message.content
         prompt_queue.put(prompt_to_inject)
 
         print(conversation_history)
@@ -134,7 +140,11 @@ def handle_closing():
     global prompt_to_inject
     global conversation_history
 
-    message = "SYSTEM: Hello Fiosa, this is the System. The user is closing you now, is there anything you would like to add to your long term memories? Reply only with the memories themselves like this 'Memory: <the memory>', nothing else as it will be added directly into your memory."
+    if (conversation_history == ''):
+        root.destroy()
+        return
+
+    message = "System: Hello Fiosa, this is the System. The user is closing you now, is there anything from the conversation you would like to add to your long term memories? Reply only with the memories themselves like this 'Memory: <the current date> <the memory>', nothing else as your response will be added directly into your memory database. Also, please only add this latest memory, don't write all of them again."
     chat_window.chat_log.insert(tk.END, "\nFiosa: Goodbye, please wait while I save my memories of this conversation :)")
     chat_window.message_entry.delete(0, tk.END)
     conversation_history = conversation_history + "\n" + "User: " + message
@@ -142,10 +152,19 @@ def handle_closing():
     completion = run_prompt(prompt_to_inject, conversation_history, "gpt-3.5-turbo")
 
     longterm_memories_file_write = open("LongTermMemories.txt", 'w')
-    longterm_memories_file_write.write(completion.choices[0].message.content + "\n") # Save to Fiosa's long-term memory.
+    longterm_memories_file_write.write(longterm_memories + completion.choices[0].message.content + "\n") # Save to Fiosa's long-term memory.
     longterm_memories_file_write.close()
 
     root.destroy()
+
+
+root.tk_setPalette(background='#1E1E1E', foreground='white',
+                   activeBackground='gray30', activeForeground='white', 
+                   highlightBackground='#1E1E1E', highlightColor='white')
+
+style = ttk.Style()
+
+
 
 root.protocol("WM_DELETE_WINDOW", handle_closing)
 root.mainloop()
